a8b49bf57d54efb368ea0151011affabeb381f6e
Nandkishore Geete <96770989+iamNickDev@users.noreply.github.com>
2023-09-18 07:05:19 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+//defile variables.
+
+var accountId = 12345;
+let accountEmail = "nk@gmail.com";
+accountCity = "indore"
+
+console.table([accountId, accountEmail]);
+
+/* 
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict";   // treat all code as a newer version
+
+
+//Data Types
+
+let name = "nk"
+let age = 24
+let pass = true
+
+// numbers => 122
+// string => ""
+// boolean => true/false
+// null => standalone value (null is typeof object)
+// undefine =>  
+// symbole => unique
+
+
+console.log(typeof null)
